c23470fc89cfa78955dc346c7152afe2520e7afd
lis <lis@info2soft.com>
2019-04-24 17:26:05 +0800

    [IM]: 增加跟随跳转

--- src/i2up/Http/Client.php
+++ src/i2up/Http/Client.php
@@ -69,6 +69,7 @@ final class Client
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HEADER => true,
             CURLOPT_NOBODY => false,
             CURLOPT_CUSTOMREQUEST => $request->method,
